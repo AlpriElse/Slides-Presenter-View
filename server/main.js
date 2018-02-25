ed93dd46d251ffd6cc33573aa3b30ac5d6df7528
AlpriElse <alprielse@gmail.com>
2018-02-24 21:33:51 -0600

    Pushed data from socket to SlideProgress collection

--- server/main.js
+++ server/main.js
@@ -1,13 +1,32 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/slideProgress.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+import {SlideProgress} from '../imports/api/slideProgress.js';
+
+var fiber = require('fibers');
 
 var io = require('socket.io-client');
 var socket = io("http://localhost:5000");
 
+
+Meteor.startup(() => {
+
+});
+
 socket.on("connect", () => {
   console.log("Meteor Server connected to Socket Cluster.");
-})
+});
+
+socket.on("updateProgress", (data) => {
+  console.log("student: ", data.student);
+  console.log("slide: ", data.slideID);
+  insertToSlideProgress(data);
+
+});
+
+var insertToSlideProgress = Meteor.bindEnvironment(function(data) {
+  SlideProgress.insert({
+    student: data.student,
+    slideID: data.slideID
+  });
+});
