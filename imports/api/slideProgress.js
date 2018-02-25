d3f274c25811a05ac21d90df06616134d9e8b98a
AlpriElse <alprielse@gmail.com>
2018-02-24 20:52:30 -0600

    Connected sandbox socket cluster server to Presenter View app

--- imports/api/slideProgress.js
+++ imports/api/slideProgress.js
@@ -2,7 +2,7 @@ import {Mongo} from 'meteor/mongo';
 
 export const SlideProgress = new Mongo.Collection('slideProgress');
 
-/*SlideProgress.schema = new SimpleSchema({
+SlideProgress.schema = new SimpleSchema({
   student: {type: String},
   slideID: {type: String}
-});*/
+});
--- server/main.js
+++ server/main.js
@@ -4,3 +4,10 @@ import '../imports/api/slideProgress.js';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+var io = require('socket.io-client');
+var socket = io("http://localhost:5000");
+
+socket.on("connect", () => {
+  console.log("Meteor Server connected to Socket Cluster.");
+})
